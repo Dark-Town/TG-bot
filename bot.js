32155710e41681c3c62950317d9d5c0648d3412e
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-22 10:43:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your bot token from BotFather
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = 'YOUR_TELEGRAM_BOT_TOKEN'; // Make sure to replace this with your actual token
 
 // Create a bot that uses 'polling' to fetch new updates
-var bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
-// Listen for any kind of message. There are different kinds of messages.
-bot.onText(//start/, function (msg) {
-    var chatId = msg.chat.id;
+// Listen for the /start command
+bot.onText(//start/, (msg) => {
+    
 
     // Send a welcome message and a link to your channel
-    var welcomeMessage = `
+    
 Welcome to our bot! 🎉
 To get started, join our channel: [Your Channel Name](https://t.me/yourchannelusername)
 `;
@@ -20,9 +20,8 @@ To get started, join our channel: [Your Channel Name](https://t.me/yourchannelus
 });
 
 // Optional: Add more commands or functionalities as needed
-bot.onText(//help/, function (msg) {
-    var chatId = msg.chat.id;
-    var helpMessage = `
+bot.onText(//help/, (msg) => {
+    
 Here are some commands you can use:
 /start - Start the bot and get a link to our channel
 /help - Get help information

02b17fc52977b6cedfdf28c805820014dcf9230a
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-22 10:40:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,28 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your bot token from BotFather
-const token = '7827301147:AAGOSpd2qTtGvMWTTSCNoJtP0AfkkjYCh68';
+var token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+var bot = new TelegramBot(token, { polling: true });
 
 // Listen for any kind of message. There are different kinds of messages.
-bot.onText(//start/, (msg) => {
-    
+bot.onText(//start/, function (msg) {
+    var chatId = msg.chat.id;
 
     // Send a welcome message and a link to your channel
-    const welcomeMessage = `
+    var welcomeMessage = `
 Welcome to our bot! 🎉
-To get started, join our channel: [Your Channel Name](https://t.me/tcronebhackx)
+To get started, join our channel: [Your Channel Name](https://t.me/yourchannelusername)
 `;
 
     bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
 });
 
 // Optional: Add more commands or functionalities as needed
-bot.onText(//help/, (msg) => {
-    
-    const helpMessage = `
+bot.onText(//help/, function (msg) {
+    var chatId = msg.chat.id;
+    var helpMessage = `
 Here are some commands you can use:
 /start - Start the bot and get a link to our channel
 /help - Get help information
